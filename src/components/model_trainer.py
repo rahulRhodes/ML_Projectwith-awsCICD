cce461f2b847ea43b15c9478d92ddd62236004ad
Rahul Singh <rahul9827193441@gmail.com>
2025-08-19 23:11:49 +0530

    Hyperparameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -88,7 +88,7 @@ class ModelTrainer:
                 
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             #to get the best model score
             best_model_score=max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,11 @@ import sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 import dill
 import pickle
 from sklearn.metrics import r2_score
+from src.logger import logging
 
 
 
@@ -23,12 +25,23 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            logging.info(f'Best Params for {model}:{gs.best_params_}')
+            logging.info(f'Best CV score  for {model}:{gs.best_score_}')
+
+            
+
+            model.set_params(**gs.best_params_)
+            
 
             model.fit(X_train,y_train)
 
